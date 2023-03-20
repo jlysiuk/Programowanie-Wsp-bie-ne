6ee32d73b94c882b4ef4cde75f15523388795d48
Jakub Łysiuk <242457@edu.p.lodz.pl>
2023-03-20 01:49:06 +0100

    zadanie 0 done

---
+++ Zadanie_0/Zadanie_0.UnitTests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Zadanie_0;
+
+namespace Zadanie_0.UnitTests
+{
+    public class ProgramTests
+    {
+        [Test]
+        public void Main_PrintsHelloWorld()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer); // wczytuje output konsoli do zmiennej writer
+
+            // Act
+            Program.Main(new string[0]);
+            var output = writer.ToString().Trim();
+
+            // Assert
+            Assert.AreEqual("Hello world", output);
+        }
+    }
+}
---
+++ Zadanie_0/Zadanie_0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zadanie_0
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
