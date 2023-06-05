0a1838a936c3afe82f6830a9e25564ea9e396c8e
Jakub Łysiuk <242457@edu.p.lodz.pl>
2023-06-05 13:27:21 +0200

    etap 3 final

--- Etap1_2/Data/DataAPI.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Data {
-    public interface DataAPI {                     //API, ktore implementuje warstwa danych
-        void add(Ball newBall);
-        void remove(int index);
-        void clear();
-        Ball get(int index);
-        int size();
-    }
-}
